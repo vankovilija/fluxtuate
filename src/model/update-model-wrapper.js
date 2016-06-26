aaf62496ce3685d0bfe2a96ea7d7b6c1be34f1dc
Ilija Vankov <i.vankov@axeltra.com>
2016-06-26 14:49:18 +0200

    add missing wrapper model vars

--- src/model/update-model-wrapper.js
+++ src/model/update-model-wrapper.js
@@ -2,12 +2,12 @@ import {model, checkDestroyed, updateable} from "./_internals"
 import ModelWrapper from "./model-wrapper"
 
 export default class UpdateModelWrapper extends ModelWrapper{
-    constructor() {
-        super();
+    constructor(wrappedModel, holderContext) {
+        super(wrappedModel, holderContext);
 
         this[updateable] = true;
     }
-    
+
     update(data) {
         this[checkDestroyed]();
         this[model].update(data);
